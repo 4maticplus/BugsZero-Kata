a7ed81d5e69bda9fa7c0d5c5a91b3713630b05e5
Mom Nd <126071183+4maticplus@users.noreply.github.com>
2024-06-21 11:57:02 +0200

    Update GameTest.java

--- java/src/test/java/com/adaptionsoft/games/trivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/GameTest.java
@@ -24,4 +24,44 @@ public class GameTest {
         Approvals.verify(resultStream.toString());
 
 	}
+
+	@Test
+        public void player_does_not_get_out_of_penalty_box() {
+		
+        Game game = new Game(Players.aNew("P1", "P2").get());
+
+        get_to_penalty_box(game); //p1
+        assertTrue(game.isFirstPlayerInPenalty());
+        game.wasCorrectlyAnswered(); //p2
+        get_out_of_penalty_box(game);
+        }
+
+	@Test
+        public void does_not_run_out_of_questions() {
+        
+	Game game = new Game(Players.aNew("P1", "P2").get());
+
+        for (int i = 0; i < totalNumberOfInitialQuestions() + 1; i++) {
+            consumeOneQuestion(game);
+        }
+		
+        }
+
+        private void consumeOneQuestion(Game game) {
+         game.roll(1);
+         game.wrongAnswer();
+        }
+
+        private int totalNumberOfInitialQuestions() {
+         return 50 * 4;
+        }
+
+        private void get_to_penalty_box(Game game) {
+          game.wrongAnswer();
+        }
+
+        private void get_out_of_penalty_box(Game game) {
+          game.roll(1);
+          game.wasCorrectlyAnswered();
+        }
 }
